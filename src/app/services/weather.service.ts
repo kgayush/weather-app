bb03fe0d432e8931dd87ab7c892484f6e503033d
agga1211 <54178133+agga1211@users.noreply.github.com>
2022-06-21 15:07:54 +0530

    Heroku setup discarded

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -12,10 +12,10 @@ export class WeatherService {
   constructor(private http: HttpClient) { }
 
   getWeatherData(cityName: string): Observable<WeatherData> {
-    return this.http.get<WeatherData>('https://community-open-weather-map.p.rapidapi.com/weather', {
+    return this.http.get<WeatherData>(environment.weatherApiBaseUrl, {
       headers: new HttpHeaders()
-      .set('X-RapidAPI-Host', 'community-open-weather-map.p.rapidapi.com')
-      .set('X-RapidAPI-Key', '08b8c36c9cmshf10716130bac52bp109bcbjsn302d440a0b61'),
+      .set(environment.XRapidApiHostHeaderName, environment.XRapidApiHostHeaderValue)
+      .set(environment.XRapidApiKeyHeaderName, environment.XRapidApiKeyHeaderValue),
       params: new HttpParams()
       .set('q', cityName)
       .set('units', 'metric')

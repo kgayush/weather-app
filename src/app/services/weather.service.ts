022226daed641ef0ae0f8be8b42d6b00e2a926a9
agga1211 <54178133+agga1211@users.noreply.github.com>
2022-06-21 14:51:32 +0530

    Environment variables exposed

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -12,10 +12,10 @@ export class WeatherService {
   constructor(private http: HttpClient) { }
 
   getWeatherData(cityName: string): Observable<WeatherData> {
-    return this.http.get<WeatherData>(environment.weatherApiBaseUrl, {
+    return this.http.get<WeatherData>('https://community-open-weather-map.p.rapidapi.com/weather', {
       headers: new HttpHeaders()
-      .set(environment.XRapidApiHostHeaderName, environment.XRapidApiHostHeaderValue)
-      .set(environment.XRapidApiKeyHeaderName, environment.XRapidApiKeyHeaderValue),
+      .set('X-RapidAPI-Host', 'community-open-weather-map.p.rapidapi.com')
+      .set('X-RapidAPI-Key', '08b8c36c9cmshf10716130bac52bp109bcbjsn302d440a0b61'),
       params: new HttpParams()
       .set('q', cityName)
       .set('units', 'metric')
